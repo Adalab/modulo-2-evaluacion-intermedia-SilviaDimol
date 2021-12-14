67dc7021c08b921f22e8d0900dad8339f34c3076
Silvia Dimol <silviadmolina@gmail.com>
2021-12-14 10:52:44 +0100

    change js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Variables globales
+/*//Variables globales
 
 const button = document.querySelector('.js-button');
 const playerInput = document.querySelector('.js-playerInput');
@@ -30,5 +30,54 @@ function handleClickPlay(event) {
     result()
 }
 //Listeners
-button.addEventListener('click', handleClickPlay);
+button.addEventListener('click', handleClickPlay); */
 
+"use strict";
+// elementos HTML
+const valueSelection = document.querySelector('js-valueSelection');
+const button = document.querySelector('js-button');
+const message = document.querySelector('js-message');
+const playerInput = document.querySelector('js-playerInput');
+const computerInput = document.querySelector('js-computerInput');
+
+
+"use strict";
+
+// Constantes generales
+const valueSelection = document.querySelector('js-valueSelection');
+const button = document.querySelector('js-button');
+const message = document.querySelector('js-message');
+const playerResult = document.querySelector('js-playerResult');
+const computerResult = document.querySelector('js-computerResult');
+
+// funciones
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+function computerPlay() {
+    let computerValue = getRandomNumber(10);
+    console.log(computerValue);
+    if (computerValue < 3) {
+        computerValue = "piedra";
+    } else if (computerValue >= 3 && computerOption <= 6) {
+        computerValue = "papel";
+    } else {
+        computerValue = "tijera";
+    }
+    return computerValue;
+  }
+  function comparePlayerOption() {
+    const userOption = valueSelect.value;
+    const computerOption = computerPlay();
+    if (userOption === computerOption()) {
+      console.log(`The number is... ${getRandomNumber()}`);
+    } else {
+      console.log(`try again`);
+    }
+  }
+function handleClickBtn() {
+    // event.preventDefault();
+    computer();
+}
+// operaciones
+button.addEventListener('click', handleClickBtn);
