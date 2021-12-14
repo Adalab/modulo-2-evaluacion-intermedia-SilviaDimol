c691f513f0b983c0487857686931adf06494107e
Silvia Dimol <silviadmolina@gmail.com>
2021-12-14 11:01:12 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
 "use strict";
 
-// Constantes generales
-const valueSelection = document.querySelector('js-valueSelection');
-const button = document.querySelector('js-button');
-const message = document.querySelector('js-message');
-const playerResult = document.querySelector('js-playerResult');
-const computerResult = document.querySelector('js-computerResult');
+// Constantes globales
+const button = document.querySelector('.js-button');
+const valueSelection = document.querySelector('.js-valueSelection');
 
-// Funciones
+//Funciones
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-  }
-function computerPlay() {
-    let computerValue = getRandomNumber(10);
-    console.log(computerValue);
-    if (computerValue < 3) {
-        computerValue = "piedra";
-    } else if (computerValue >= 3 && computerOption <= 6) {
-        computerValue = "papel";
-    } else {
-        computerValue = "tijera";
-    }
-    return computerValue;
-  }
-  function comparePlayerOption() {
-    const userOption = valueSelect.value;
-    const computerOption = computerPlay();
-    if (userOption === computerOption()) {
-      console.log(`The number is... ${getRandomNumber()}`);
-    } else {
-      console.log(`try again`);
-    }
-  }
-function handleClickBtn() {
-    // event.preventDefault();
-    computer();
 }
-// Eventos
-button.addEventListener('click', handleClickBtn);
+function displayMessage() {
+    const userValue = userSelection.value;
+    console.log(`Tu movimiento es: ` + userValue);
+}
+
+
+function getComputerValue() {
+    const randomNumber = getRandomNumber(10);
+    let computerValue = '';
+    if (randomNumber < 3) {
+        computerValue = 'Piedra';
+    } else if (randomNumber >= 6) {
+        computerValue = 'Papel';
+    } else if (randomNumber > 6) {
+        computerValue = 'Tijera';
+    }
+    console.log(`El movimiento de la computadora es: ` + computerValue);
+}
+
+
+function handleClickPlay(event) {
+    event.preventDefault();
+    displayMessage()
+    changePoints()
+}
+//Listeners
+button.addEventListener('click', handleClickPlay);
