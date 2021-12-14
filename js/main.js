df5c1b3f84ec75c46c9516e138358a350c4a00ec
Silvia Dimol <silviadmolina@gmail.com>
2021-12-14 10:55:29 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-'use strict';
-
-/*//Variables globales
-
-const button = document.querySelector('.js-button');
-const playerInput = document.querySelector('.js-playerInput');
-
-//Funciones
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-}
-function displayMessage() {
-    const playerValue = playerInput.value;
-    console.log(`Tu movimiento es: ` + playerValue);
-}
-function changePoints() {
-    const randomNumber = getRandomNumber(10);
-    if (randomNumber < 3) {
-        let computerValue = 'Piedra';
-    } else if (randomNumber >= 6) {
-        let computerValue = 'Papel';
-    } else if (randomNumber > 6) {
-        let computerValue = 'Tijera';
-    }
-    console.log(`El movimiento del ordenador es: ` + computerValue);
-}
-function handleClickPlay(event) {
-    event.preventDefault();
-    message()
-    result()
-}
-//Listeners
-button.addEventListener('click', handleClickPlay); */
-
-"use strict";
-// elementos HTML
-const valueSelection = document.querySelector('js-valueSelection');
-const button = document.querySelector('js-button');
-const message = document.querySelector('js-message');
-const playerInput = document.querySelector('js-playerInput');
-const computerInput = document.querySelector('js-computerInput');
-
-
 "use strict";
 
 // Constantes generales
@@ -50,7 +7,7 @@ const message = document.querySelector('js-message');
 const playerResult = document.querySelector('js-playerResult');
 const computerResult = document.querySelector('js-computerResult');
 
-// funciones
+// Funciones
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
@@ -79,5 +36,5 @@ function handleClickBtn() {
     // event.preventDefault();
     computer();
 }
-// operaciones
+// Eventos
 button.addEventListener('click', handleClickBtn);
