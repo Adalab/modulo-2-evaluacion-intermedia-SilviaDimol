5105f317147f6cb746af1f8548f73860232d8718
Silvia Dimol <silviadmolina@gmail.com>
2021-12-14 11:26:16 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,81 @@
 "use strict";
 
-// Constantes globales
-const button = document.querySelector('.js-button');
-const valueSelection = document.querySelector('.js-valueSelection');
+// Constantes
+const playerSelection = document.querySelector(".js-playerSelection");
+const button = document.querySelector(".js-button");
+const message = document.querySelector(".js-message");
+const form = document.querySelector(".js-form");
+const tries = document.querySelector(".js-tries");
 
-//Funciones
+// Crear número random y número de intentos
+const max = 9;
+let triesNumber = 10;
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
-function displayMessage() {
-    const userValue = userSelection.value;
-    console.log(`Tu movimiento es: ` + userValue);
+let r = getRandomNumber(max);
+let computer =  Game();
+
+// Asociaciones número-objeto
+function Game() {
+  if (r < 3) {
+    console.log("El ordenador escoge la piedra");
+    return "piedra";
+  } else if (r <= 6) {
+    console.log("El ordenador escoge el papel");
+    return "papel";
+  } else {
+    console.log("El ordenador escoge la tijera");
+    return "tijera";
+  }
 }
-
-
-function getComputerValue() {
-    const randomNumber = getRandomNumber(10);
-    let computerValue = '';
-    if (randomNumber < 3) {
-        computerValue = 'Piedra';
-    } else if (randomNumber >= 6) {
-        computerValue = 'Papel';
-    } else if (randomNumber > 6) {
-        computerValue = 'Tijera';
+// Juego en sí
+function checkNumber() {
+  const player = playerSelection.value;
+  
+  if (player === "piedra") {
+    
+    if ( computer === "piedra") {
+      message.innerHTML = "empate";
+    } else if ( computer === "tijera") {
+      message.innerHTML = "has ganado";
+    } else if ( computer === "papel") {
+      message.innerHTML = "has perdido";
     }
-    console.log(`El movimiento de la computadora es: ` + computerValue);
-}
+  }
+   
+    else if (player === "tijera") {
+      if ( computer === "piedra") {
+        message.innerHTML = "has perdido";
+      } else if ( computer === "tijera") {
+        message.innerHTML = "empate";
+      } else if ( computer === "papel") {
+        message.innerHTML = "has ganado";
+      }
+} 
+ else if (player === "papel") {
+      if ( computer === "piedra") {
+        message.innerHTML = "has perdido";
+      } else if ( computer === "tijera") {
+        message.innerHTML = "empate";
+      } else if ( computer === "papel") {
+        message.innerHTML = "has ganado";
+      }
+    }
+  }
 
-
-function handleClickPlay(event) {
-    event.preventDefault();
-    displayMessage()
-    getComputerValue()
+//  Eventos
+btn.addEventListener("click", handleClick);
+function handleClick(ev) {
+  ev.preventDefault();
+  if (triesNumber > 0) {
+    triesNumber--;
+    tries.innerHTML = `Tienes ${triesNumber} intentos`;
+    checkNumber();
+    r = getRandomNumber(max);
+    computer =  Game();
+  }
+   else {
+    tries.innerHTML = "No te quedan más intentos";
+  }
 }
-//Listeners
-button.addEventListener('click', handleClickPlay);
